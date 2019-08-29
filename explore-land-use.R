4ea47164a871c662608e20dfbe77ff41908bd458
Raven <raven.i.mcknight@gmail.com>
2019-08-29 08:59:49 -0500

    update explore script

--- explore-land-use.R
+++ explore-land-use.R
@@ -11,6 +11,8 @@ library(gridExtra)
 library(parsetR)
 library(forcats)
 library(stringr)
+library(leaflet)
+library(htmltools)
 
 # block groups
 counties <- c("Anoka", "Carver", "Dakota", "Hennepin", "Ramsey", "Scott", "Washington") 
@@ -26,8 +28,10 @@ file.remove(loc)
 lu <- readOGR(file.path(tempdir(), 'lu'), layer = 'GeneralizedLandUseHistorical', stringsAsFactors = FALSE)
 lusf <- st_as_sf(lu)
 
+switch_sf <- lusf %>% filter(LUSE2010 == 210 & LUSE2016 != 210)
+
 # easier to work with 
-ludt <- setDT(lusf)
+ludt <- data.table(lusf)
 
 # summaries ---------------------------
 unique(ludt$Dscrpt2010)
@@ -53,3 +57,55 @@ tmp <- tmp[order(-N)]
 parset(tmp, dimensions = c('Origin', 'Destination'),
        value = htmlwidgets::JS("function(d){return d.N}"),
        tension = 0.5)
+
+# Which land use types are changing overall? --------------
+
+# get counts
+c10 <- ludt[, .N, by = Dscrpt2010]
+c10[, desc := factor(str_trim(Dscrpt2010))]
+c10[, Dscrpt2010 := NULL]
+
+c16 <- ludt[, .N, by = Dscrpt2016]
+c16[, desc := factor(str_trim(Dscrpt2016))]
+c16[, Dscrpt2016 := NULL]
+
+c <- c16[c10, on = 'desc']
+colnames(c) <- c('count10', 'desc', 'count16')
+c[, change := count16-count10]
+
+ggplot(c) +
+  geom_bar(aes(x = desc, y = change, fill = change > 0), stat = 'identity') +
+  theme_minimal() + coord_flip() +
+  theme(legend.position = 'none', plot.title = element_text(hjust = 0.5)) +
+  labs(x = 'land use', title = 'Change in land use 2010-2016')
+
+# Spatially, where is undev becoming dev? -----------------
+switch_sf <- st_transform(switch_sf, 4326) # leaflet does not like utm
+
+#lusepal <- colorFactor(palette = 'Set3', levels = levels(switch_sf$Dscrpt2016))
+
+labs <- sprintf(
+  "Now categorized as %s",
+  switch_sf$Dscrpt2016
+) %>% lapply(htmltools::HTML)
+
+leaflet(switch_sf) %>% addTiles() %>%
+  addPolygons(weight = 4, color = 'gray', fillOpacity = 0.8, label = labs)
+
+# different in urban core?
+uc <- block_groups('MN', county = c('Hennepin', 'Ramsey'), year = 2016)
+uc <- st_transform(uc, 4326)
+
+uc_undev <- st_join(switch_sf, uc, st_within)
+# sum(is.na(uc_undev$STATEFP))  # not all NA
+uc_undev_dt <- data.table(uc_undev)
+uc_undev_dt[is.na(STATEFP), urbancore := FALSE]
+uc_undev_dt[!is.na(STATEFP), urbancore := TRUE]
+
+ggplot(uc_undev_dt) +
+  geom_bar(aes(x=fct_infreq(Dscrpt2016), fill = urbancore), stat = 'count') +
+  theme(axis.text = element_text(angle = 90)) +
+  theme_minimal() + coord_flip() +
+  labs(x = '2016 use', title = '2016 uses of undeveloped 2010 land')
+
+# was anything uncategorized? -----------------------------
